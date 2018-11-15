945cddd53b2f1dc13bb3ed31af139e906ab02fd6
Youngmin Kim <ymjskk96@gmail.com>
2018-11-14 18:53:22 -0800

    log in works

--- js/log_in.js
+++ js/log_in.js
@@ -1,6 +1,17 @@
 var login = [
     {
-        username: "pizzacat",
-        password: "eatpizza"
+        user: "pizzacat",
+        pw: "eatpizza"
     }
 ]
+
+function validate() {
+    var user = document.getElementById("user").value
+    var pw = document.getElementById("pw").value
+
+    for (i = 0; i < login.length; i ++){
+        if (user == login[i].user && pw == login[i].pw) {
+            window.location = "index.html";
+        }
+    }
+}
