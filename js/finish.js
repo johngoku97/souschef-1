b13208712b80bd70c5efbaf4436a3986b90c0a23
Jonathan Wong <jkw006@ucsd.edu>
2018-11-15 18:53:35 -0800

    idk what i did lol

---
+++ js/finish.js
@@ -0,0 +1,9 @@
+$("favorite").on("click", function() {
+  var el = $(this);
+  if (el.text() == el.data("text-swap")) {
+    el.text(el.data("text-original"));
+  } else {
+    el.data("text-original", el.text());
+    el.text(el.data("text-swap"));
+  }
+});
