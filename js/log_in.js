c1ac57fb55d3e732fcf14b8cc47de9d331c642f8
Youngmin Kim <ymjskk96@gmail.com>
2018-11-15 16:50:15 -0800

    added data

--- js/log_in.js
+++ js/log_in.js
@@ -2,8 +2,24 @@ var login = [
     {
         user: "pizzacat",
         pw: "eatpizza"
+        
     }
 ]
+//names are subject to change
+var settings = [
+    {
+        diet: "Keto",
+        diet: "Paleo",
+        diet: "Vegan",
+        allergen: "Dairy",
+        allergen: "Gluten",
+        allergen: "Eggs",
+        allergen: "Shell",
+        allergen: "Nuts",
+        allergen: "Soy"
+    }
+]
+
 
 function validate() {
     var user = document.getElementById("user").value
@@ -12,6 +28,18 @@ function validate() {
     for (i = 0; i < login.length; i ++){
         if (user == login[i].user && pw == login[i].pw) {
             window.location = "index.html";
+            return true;
+        }
+        else {
+            return false;
         }
     }
+}
+
+function login() {
+         if (validate() == true) {
+             sessionStorage.setItem("log_in", document.getElementById("user").value);
+         }
+
+
 }
